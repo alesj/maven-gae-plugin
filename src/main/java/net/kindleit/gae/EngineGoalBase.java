4a3f29bcf1c6261863325f6db04c79d2c213f6f7
Rodolfo Hansen <kryptt@gmail.com>
2009-09-02 19:37:39 +0000

    Halfway through working out  just getting the full SDK zip file, unloading and workig with that at runtime.

--- src/main/java/net/kindleit/gae/EngineGoalBase.java
+++ src/main/java/net/kindleit/gae/EngineGoalBase.java
@@ -1,24 +1,43 @@
 package net.kindleit.gae;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.settings.Settings;
 
-import com.google.appengine.repackaged.com.google.common.collect.Lists;
-import com.google.appengine.tools.KickStart;
-import com.google.appengine.tools.admin.AppCfg;
+import sun.misc.JarFilter;
 
 /** Base MOJO class for working with the Google App Engine SDK.
  *
  * @author rhansen@kindleit.net
  */
 public abstract class EngineGoalBase extends AbstractMojo {
+  public static final String PLUGIN_VERSION="0.9";
 
-  public static final String PLUGIN_VERSION="0.3";
   protected static final String[] ARG_TYPE = new String[0];
+
+  private static final String APPCFG_CLASS =
+    "com.google.appengine.tools.admin.AppCfg";
+  private static final String KICKSTART_CLASS =
+    "com.google.appengine.tools.admin.KickStart";
+
+  private static final String SDK_LIBS_NOTFOUND =
+    "AppEngine tools SDK could not be found";
+  private static final String SDK_APPCFG_NOTFOUND =
+    "AppCfg Class not found at: " + APPCFG_CLASS;
+  private static final String SDK_KICKSTART_NOTFOUND =
+    "KickStart Class not found at: " + KICKSTART_CLASS;
+
+  private static URLClassLoader classLoader;
+
 
   /** The Maven project reference.
    *
@@ -42,13 +61,6 @@ public abstract class EngineGoalBase extends AbstractMojo {
    * @required
    */
   protected String appDir;
-
-  /** Specifies where the Google App Engine SDK is located.
-   *
-   * @parameter expression="${appengine.sdk.root}"
-   * @required
-   */
-  protected String sdkDir;
 
   /** Split large jar files (> 10M) into smaller fragments.
    *
@@ -86,6 +98,7 @@ public abstract class EngineGoalBase extends AbstractMojo {
    */
   protected boolean passIn;
 
+
   /** Passes command to the Google App Engine AppCfg runner.
   *
   * @param command command to run through AppCfg
@@ -94,22 +107,26 @@ public abstract class EngineGoalBase extends AbstractMojo {
   protected final void runAppCfg(final String command,
       final String ... commandArguments) {
 
-    final List<String> args = getAppCfgArgs();
-    args.addAll(Lists.asList(command, commandArguments));
+    final List<String> args = new ArrayList<String>();
+    args.addAll(getAppCfgArgs());
+    args.add(command);
+    args.addAll(Arrays.asList(commandArguments));
 
     AppCfg.main(args.toArray(ARG_TYPE));
   }
 
   /** Passes command to the Google App Engine KickStart runner.
-   *
-   * @param startClass command to run through KickStart
-   * @param commandArguments arguments to the KickStart command.
-   */
+  *
+  * @param startClass command to run through KickStart
+  * @param commandArguments arguments to the KickStart command.
+  */
   protected final void runKickStart(final String startClass,
-      final String ... commandArguments) {
+    final String ... commandArguments) {
 
-    final List<String> args = Lists.asList(startClass, commandArguments);
+    final List<String> args = new ArrayList<String>();
+    args.add(startClass);
     args.addAll(getCommonArgs());
+    args.addAll(Arrays.asList(commandArguments));
 
     KickStart.main(args.toArray(ARG_TYPE));
   }
@@ -155,6 +172,41 @@ public abstract class EngineGoalBase extends AbstractMojo {
     if (var != null && var.length() > 0) {
       args.add(key + var);
     }
+  }
+
+  protected Class<?> getEngineClass(final String clsName,
+      final String clsErrorMsg) throws MojoExecutionException {
+    try {
+      return getClassLoader().loadClass(clsName);
+    } catch (final ClassNotFoundException e) {
+      throw new MojoExecutionException(clsErrorMsg, e);
+    }
+  }
+
+  private ClassLoader getClassLoader() throws MojoExecutionException {
+    synchronized (EngineGoalBase.class) {
+      if (classLoader != null) {
+        return classLoader;
+      }
+    }
+    synchronized (EngineGoalBase.class) {
+      try {
+        final List<URL> urls = new ArrayList<URL>();
+
+        for(final File jar : getSdkLibDir().listFiles(new JarFilter())) {
+          urls.add(jar.toURI().toURL());
+        }
+
+        classLoader = new URLClassLoader(urls.toArray(new URL[0]));
+      } catch (final MalformedURLException e) {
+        throw new MojoExecutionException(SDK_LIBS_NOTFOUND, e);
+      }
+      return classLoader;
+    }
+  }
+
+  private File getSdkLibDir() {
+    return null;
   }
 
 }
