b193a375e13dc329055da7977dd3623a7452a8a1
Rodolfo Hansen <kryptt@gmail.com>
2009-12-18 14:34:58 +0000

    Updating License information

--- src/main/java/net/kindleit/gae/UpdateGoal.java
+++ src/main/java/net/kindleit/gae/UpdateGoal.java
@@ -1,11 +1,16 @@
-/*
- * Copyright 2008 Kindleit Technologies. All rights reserved. This file, all
- * proprietary knowledge and algorithms it details are the sole property of
- * Kindleit Technologies unless otherwise specified. The software this file
- * belong with is the confidential and proprietary information of Kindleit
- * Technologies. ("Confidential Information"). You shall not disclose such
- * Confidential Information and shall use it only in accordance with the terms
- * of the license agreement you entered into with Kindleit.
+/* Copyright 2009 Kindleit.net Software Development
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package net.kindleit.gae;
 
@@ -25,6 +30,8 @@ public class UpdateGoal extends EngineGoalBase {
 
   /** Create or update an app version.
    * This goal uploads your web application to the google app engine server.
+   * @throws MojoExecutionException Unthrown
+   * @throws MojoFailureException Unthrown
    */
   public void execute() throws MojoExecutionException, MojoFailureException {
     getLog().info("Updating Google App Engine Server...");
