d8a40f4561e8331b6874b006c4ae95562512f44d
Rodolfo Hansen <kryptt@gmail.com>
2010-08-17 22:47:12 +0000

    upgrading to google appengine 1.3.6

--- maven-gae-plugin/gae-deploy.sh
+++ maven-gae-plugin/gae-deploy.sh
@@ -7,15 +7,17 @@
 #   http://code.google.com/p/googleappengine/issues/detail?id=1296
 #
 
+SDK_VERSION=1.3.6
+
 if [ "$1" == "" ]
 then
-    echo "usage: $0 /path/to/appengine-java-sdk-1.3.5 [install|deploy]"
+    echo "usage: $0 /path/to/appengine-java-sdk-$SDK_VERSION [install|deploy]"
     exit 1
 fi
 
 if [ "$2" == "" ]
 then
-    echo "usage: $0 /path/to/appengine-java-sdk-1.3.5 [install|deploy]"
+    echo "usage: $0 /path/to/appengine-java-sdk-$SDK_VERSION [install|deploy]"
     exit 1
 fi
 
@@ -23,18 +25,22 @@ GAE_SDK_PATH="$1"
 TASK="$2"
 URL="svn:https://maven-gae-plugin.googlecode.com/svn/repository"
 
-mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/user/appengine-api-1.0-sdk-1.3.5.jar -DgroupId=com.google.appengine -DartifactId=appengine-api-1.0-sdk -Dversion=1.3.5 -DgeneratePom=true -Dpackaging=jar
+mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/user/appengine-api-1.0-sdk-$SDK_VERSION.jar -DgroupId=com.google.appengine -DartifactId=appengine-api-1.0-sdk -Dversion=$SDK_VERSION -DgeneratePom=true -Dpackaging=jar
  
-mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/user/appengine-api-labs-1.3.5.jar -DgroupId=com.google.appengine -DartifactId=appengine-api-labs -Dversion=1.3.5 -DgeneratePom=true -Dpackaging=jar
+mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/docs/javadoc/appengine-api-1.0-sdk-$SDK_VERSION-javadoc.jar -DgroupId=com.google.appengine -DartifactId=appengine-api-1.0-sdk -Dversion=$SDK_VERSION -DgeneratePom=true -Dclassifier=javadoc -Dpackaging=jar
  
-mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/appengine-tools-api.jar -DgroupId=com.google.appengine -DartifactId=appengine-tools-api -Dversion=1.3.5 -DgeneratePom=true -Dpackaging=jar
+mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/user/appengine-api-labs-$SDK_VERSION.jar -DgroupId=com.google.appengine -DartifactId=appengine-api-labs -Dversion=$SDK_VERSION -DgeneratePom=true -Dpackaging=jar
+ 
+mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/appengine-tools-api.jar -DgroupId=com.google.appengine -DartifactId=appengine-tools-api -Dversion=$SDK_VERSION -DgeneratePom=true -Dpackaging=jar
 
-mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/impl/appengine-local-runtime.jar -DgroupId=com.google.appengine -DartifactId=appengine-local-runtime -Dversion=1.3.5 -DgeneratePom=true -Dpackaging=jar
+mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/impl/appengine-local-runtime.jar -DgroupId=com.google.appengine -DartifactId=appengine-local-runtime -Dversion=$SDK_VERSION -DgeneratePom=true -Dpackaging=jar
 
-mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/impl/appengine-api-stubs.jar -DgroupId=com.google.appengine -DartifactId=appengine-api-stubs -Dversion=1.3.5 -DgeneratePom=true -Dpackaging=jar
+mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/impl/appengine-api-stubs.jar -DgroupId=com.google.appengine -DartifactId=appengine-api-stubs -Dversion=$SDK_VERSION -DgeneratePom=true -Dpackaging=jar
 
-mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/testing/appengine-testing.jar -DgroupId=com.google.appengine -DartifactId=appengine-testing -Dversion=1.3.5 -DgeneratePom=true -Dpackaging=jar
+mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/testing/appengine-testing.jar -DgroupId=com.google.appengine -DartifactId=appengine-testing -Dversion=$SDK_VERSION -DgeneratePom=true -Dpackaging=jar
+
+mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/docs/testing/javadoc/appengine-testing-javadoc.jar -DgroupId=com.google.appengine -DartifactId=appengine-testing -Dversion=$SDK_VERSION -DgeneratePom=true -Dclassifier=javadoc -Dpackaging=jar
 
 mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/user/orm/datanucleus-appengine-1.0.7.final.jar -DgroupId=com.google.appengine.orm -DartifactId=datanucleus-appengine -Dversion=1.0.7.final -DgeneratePom=true -Dpackaging=jar
 
-mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/user/appengine-jsr107cache-1.3.5.jar -DgroupId=com.google.appengine -DartifactId=jsr107cache -Dversion=1.3.5 -DgeneratePom=true -Dpackaging=jar
+mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/user/appengine-jsr107cache-$SDK_VERSION.jar -DgroupId=com.google.appengine -DartifactId=jsr107cache -Dversion=$SDK_VERSION -DgeneratePom=true -Dpackaging=jar
