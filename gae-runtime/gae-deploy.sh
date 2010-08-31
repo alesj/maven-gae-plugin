69a5204b6a50449cb6e68b726d9d08ee99e4ca3a
Rodolfo Hansen <kryptt@gmail.com>
2010-08-31 06:48:07 +0000

    updating to gae 1.3.7

--- gae-runtime/gae-deploy.sh
+++ gae-runtime/gae-deploy.sh
@@ -7,7 +7,7 @@
 #   http://code.google.com/p/googleappengine/issues/detail?id=1296
 #
 
-SDK_VERSION=1.3.6
+SDK_VERSION=1.3.7
 
 if [ "$1" == "" ]
 then
@@ -31,7 +31,7 @@ mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/docs/javadoc/appengine-api-
  
 mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/user/appengine-api-labs-$SDK_VERSION.jar -DgroupId=com.google.appengine -DartifactId=appengine-api-labs -Dversion=$SDK_VERSION -DgeneratePom=true -Dpackaging=jar
  
-mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/appengine-tools-api.jar -DgroupId=com.google.appengine -DartifactId=appengine-tools-api -Dversion=$SDK_VERSION -DgeneratePom=true -Dpackaging=jar
+mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/appengine-tools-api.jar -DgroupId=com.google.appengine -DartifactId=appengine-tools-sdk -Dversion=$SDK_VERSION -DgeneratePom=true -Dpackaging=jar
 
 mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/impl/appengine-local-runtime.jar -DgroupId=com.google.appengine -DartifactId=appengine-local-runtime -Dversion=$SDK_VERSION -DgeneratePom=true -Dpackaging=jar
 
@@ -41,6 +41,12 @@ mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/testing/appengine-testi
 
 mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/docs/testing/javadoc/appengine-testing-javadoc.jar -DgroupId=com.google.appengine -DartifactId=appengine-testing -Dversion=$SDK_VERSION -DgeneratePom=true -Dclassifier=javadoc -Dpackaging=jar
 
-mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/user/orm/datanucleus-appengine-1.0.7.final.jar -DgroupId=com.google.appengine.orm -DartifactId=datanucleus-appengine -Dversion=1.0.7.final -DgeneratePom=true -Dpackaging=jar
+mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/user/orm/jdo2-api-2.3-eb.jar -DgroupId=com.google.appengine.orm -DartifactId=jdo2-api -Dversion=2.3-eb -DgeneratePom=true -Dpackaging=jar
+
+mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/user/orm/datanucleus-appengine-1.0.7.final.jar -DgroupId=com.google.appengine.orm -DartifactId=datanucleus-appengine -Dversion=1.0.7 -DgeneratePom=true -Dpackaging=jar
+
+mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/user/orm/datanucleus-core-1.1.5.jar -DgroupId=com.google.appengine.orm -DartifactId=datanucleus-core -Dversion=1.1.5 -DgeneratePom=true -Dpackaging=jar
+
+mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/user/orm/datanucleus-jpa-1.1.5.jar -DgroupId=com.google.appengine.orm -DartifactId=datanucleus-jpa -Dversion=1.1.5 -DgeneratePom=true -Dpackaging=jar
 
 mvn $TASK:$TASK-file -Durl=$URL -Dfile=$GAE_SDK_PATH/lib/user/appengine-jsr107cache-$SDK_VERSION.jar -DgroupId=com.google.appengine -DartifactId=jsr107cache -Dversion=$SDK_VERSION -DgeneratePom=true -Dpackaging=jar
